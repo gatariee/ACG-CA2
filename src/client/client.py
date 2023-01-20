4f395eb0975aed4da4e01d2d3dde2accc1d80aae
gatari <79693291+deceptivecz@users.noreply.github.com>
2023-01-20 14:40:08 +0800

    Update client.py

--- src/client/client.py
+++ src/client/client.py
@@ -54,44 +54,11 @@ if __name__ == "__main__":
     print(f"[CLIENT] Connected to {host}:{port}")
     print(f"[CLIENT] Sending: {cmd_GET_MENU.decode()}")
     if receive_file():
-        print(f"[{cmd_GET_MENU.decode()}] OK.")
+        print(f"[GET_MENU] OK.")
     print(f"[CLIENT] Sending: {cmd_END_DAY.decode()}")
     if send_file():
-        print(f"[{cmd_END_DAY.decode()}] OK.")
+        print(f"[CLOSING] OK.")
     print(f"[CLIENT] Closing connection.")
-
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as my_socket:
-#     my_socket.connect((host, port))
-#     my_socket.sendall(cmd_GET_MENU )
-#     data = my_socket.recv(4096)
-#     #hints : need to apply a scheme to verify the integrity of data.  
-#     menu_file = open(menu_file,"wb")
-#     menu_file.write( data)
-#     menu_file.close()
-#     my_socket.close()
-# print('Menu today received from server')
-# #print('Received', repr(data))  # for debugging use
-# my_socket.close()
-
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as my_socket:
-#     my_socket.connect((host, port))
-#     my_socket.sendall(cmd_END_DAY)
-#     try:
-#         out_file = open(return_file,"rb")
-#     except:
-#         print("file not found : " + return_file)
-#         sys.exit(0)
-#     file_bytes = out_file.read(1024)
-#     sent_bytes=b''
-#     while file_bytes != b'':
-#         my_socket.send(file_bytes)
-#         sent_bytes+=file_bytes
-#         file_bytes = out_file.read(1024)
-#     out_file.close()
-#     my_socket.close()
-# print('Sale of the day sent to server')
-# my_socket.close()
-
 
 
 
