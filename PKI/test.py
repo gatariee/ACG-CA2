7d06ec3bc2d3a45c1a89a52f18476886cc759535
Zavier Lee <79693291+gatariee@users.noreply.github.com>
2023-01-27 19:43:03 +0800

    Update test.py

--- PKI/test.py
+++ PKI/test.py
@@ -1,18 +1,12 @@
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import padding
-
-# Load the certificate sent by the server
 with open("server_cert.crt", "rb") as f:
     server_cert_data = f.read()
 server_cert = x509.load_pem_x509_certificate(server_cert_data, default_backend())
-
-# Load the "client_test.crt" certificate
 with open("client_test.crt", "rb") as f:
     client_test_data = f.read()
 client_test_cert = x509.load_pem_x509_certificate(client_test_data, default_backend())
-
-# Verify the server certificate
 try:
     server_cert.public_key().verify(
         server_cert.signature,
